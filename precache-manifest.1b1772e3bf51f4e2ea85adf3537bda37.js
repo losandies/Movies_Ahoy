3d8d18d0c768ee3d34fcc6a50fb825c4a7c3c550
Brandon Newsome <brandon.i.newsome@gmail.com>
2021-10-25 16:19:13 -0400

    Updates

---
+++ precache-manifest.1b1772e3bf51f4e2ea85adf3537bda37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a5ef4300600c21bbbbe7ce50e914e5",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "3ebe4a46eecd68e45291",
+    "url": "/movie-search-app/static/css/main.7f3799cc.chunk.css"
+  },
+  {
+    "revision": "8bf66d25015996c8aaea",
+    "url": "/movie-search-app/static/js/2.a94885b2.chunk.js"
+  },
+  {
+    "revision": "6727d365f87857827f32c8a2333c549b",
+    "url": "/movie-search-app/static/js/2.a94885b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9849a325f03e03a19d",
+    "url": "/movie-search-app/static/js/3.53ec1097.chunk.js"
+  },
+  {
+    "revision": "3ebe4a46eecd68e45291",
+    "url": "/movie-search-app/static/js/main.7e4caa24.chunk.js"
+  },
+  {
+    "revision": "3846f153ea5bf5f1f020",
+    "url": "/movie-search-app/static/js/runtime-main.0ad9ac6a.js"
+  }
+]);
